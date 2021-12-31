f1f42f43bba220db9b0bfdc4172cf882489e9350
SaltyChiang <SaltyChiang@users.noreply.github.com>
2022-01-01 00:16:56 +0800

    Make root folder cleaner.

--- make_portable.ps1
+++ make_portable.ps1
@@ -37,13 +37,8 @@ DownloadFile -Uri $Packages.python.url -OutFile $Packages.python.name -Hash $Pac
 DownloadFile -Uri $Packages.vapoursynth.url -OutFile $Packages.vapoursynth.name -Hash $Packages.vapoursynth.hash
 DownloadFile -Uri $Packages.vseditor.url -OutFile $Packages.vseditor.name -Hash $Packages.vseditor.hash
 # DownloadFile -Uri $Packages.vsrepogui.url -OutFile $Packages.vsrepogui.name -Hash $Packages.vsrepogui.hash
-DownloadFile -Uri $Packages.vspreview.url -OutFile $Packages.vspreview.name -Hash $Packages.vspreview.hash
-DownloadFile -Uri $Packages.lexpr.url -OutFile $Packages.lexpr.name -Hash $Packages.lexpr.hash
+DownloadFile -Uri $Packages.vspreview.url -OutFile $Packages.vspreview.name
 DownloadFile -Uri $Packages.getpip.url -OutFile $Packages.getpip.name
-DownloadFile -Uri $Packages.ocr.url -OutFile $Packages.ocr.name -Hash $Packages.ocr.hash
-DownloadFile -Uri $Packages.imwri.url -OutFile $Packages.imwri.name -Hash $Packages.imwri.hash
-DownloadFile -Uri $Packages.subtext.url -OutFile $Packages.subtext.name -Hash $Packages.subtext.hash
-DownloadFile -Uri $Packages.vsstubs.url -OutFile $Packages.vsstubs.name
 Pop-Location
 
 
@@ -69,13 +64,8 @@ Push-Location -Path downloads
 Expand-Archive -Path $Packages.'7za'.name -DestinationPath "7za" -Force
 Expand-Archive -Path $Packages.vspreview.name -DestinationPath vspreview -Force
 # Expand-Archive -Path $Packages.vsrepogui.name -DestinationPath VSRepoGUI -Force
-Expand-Archive -Path $Packages.vsstubs.name -DestinationPath vsstubs -Force
-Expand-Archive -Path $Packages.vapoursynth.name -Destination ..\VapourSynth -Force
-Expand7Zip -Path $Packages.vseditor.name -Destination ..\VapourSynth\
-Expand7Zip -Path $Packages.lexpr.name -Destination ..\VapourSynth\vapoursynth64\plugins\
-Expand7Zip -Path $Packages.ocr.name -Destination ..\VapourSynth\vapoursynth64\coreplugins\
-Expand7Zip -Path $Packages.imwri.name -Destination ..\VapourSynth\vapoursynth64\coreplugins\
-Expand7Zip -Path $Packages.subtext.name -Destination ..\VapourSynth\vapoursynth64\coreplugins\
+Expand7Zip -Path $Packages.vapoursynth.name -Destination ..\VapourSynth -Force
+Expand7Zip -Path $Packages.vseditor.name -Destination ..\VapourSynth\VapourSynthEditor
 Pop-Location
 
 
@@ -84,8 +74,6 @@ $Requirements = Get-Item .\downloads\vspreview\vapoursynth-preview-$($Packages.v
 Set-Content -Path $Requirements (Get-Content -Path $Requirements | Select-String -Pattern 'vapoursynth' -NotMatch )
 .\VapourSynth\python.exe -m pip install -r $Requirements --no-warn-script-location
 .\VapourSynth\python.exe -m pip install flake8 black --no-warn-script-location
-.\VapourSynth\python.exe -m pip install .\downloads\vsstubs\VapourSynth-Plugins-Stub-Generator-$($Packages.vsstubs.branch)\vsstubs\ --no-warn-script-location
-.\VapourSynth\python.exe -m vsstubs install
 
 
 if ( Test-Path -Path .\VapourSynth\__pycache__ ) {
@@ -96,7 +84,7 @@ Move-Item -Path .\VapourSynth\vapoursynth.cp39-win_amd64.pyd -Destination .\Vapo
 Copy-Item -Path .\downloads\vspreview\vapoursynth-preview-$($Packages.vspreview.branch)\vspreview -Destination .\VapourSynth\Lib\site-packages\ -Recurse -Force
 # Copy-Item -Path .\downloads\VSRepoGUI\VSRepoGUI.exe -Destination .\VapourSynth\ -Force
 Copy-Item -Path .\vsrepogui.json -Destination .\VapourSynth\ -Force
-# Copy-Item -Path .\vsedit.config -Destination .\VapourSynth\ -Force
+Copy-Item -Path .\vsedit.config -Destination .\VapourSynth\VapourSynthEditor\ -Force
 New-Item -Path .\VapourSynth\vsedit.config -Force | Out-Null
 New-Item -Path .\VapourSynth\VapourSynthScripts -ItemType Directory -Force | Out-Null
 
@@ -105,16 +93,7 @@ Push-Location -Path downloads
 Remove-Item -Path 7za -Recurse -Force
 Remove-Item -Path vspreview -Recurse -Force
 # Remove-Item -Path VSRepoGUI -Recurse -Force
-Remove-Item -Path vsstubs -Recurse -Force
 Pop-Location
-
-# Remove some extra files we don't need.
-Remove-Item -Path .\VapourSynth\LICENSE.txt
-Remove-Item -Path .\VapourSynth\setup.py, .\VapourSynth\MANIFEST.in
-Remove-Item -Path .\VapourSynth\VapourSynth_portable.egg-info -Recurse
-Remove-Item -Path .\VapourSynth\vs-detect-python.bat, .\VapourSynth\vapoursynth.cp38-win_amd64.pyd
-Remove-Item -Path .\VapourSynth\README, .\VapourSynth\CHANGELOG, .\VapourSynth\LICENSE
-Remove-Item -Path .\VapourSynth\vsedit.ico, .\VapourSynth\vsedit.svg
 
 
 Write-Output "Done."

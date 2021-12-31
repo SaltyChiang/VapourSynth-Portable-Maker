f0c6b4cfbeb76ddd3876ea33ad4c7934902de4e6
SaltyChiang <jiangxiangyu@ihep.ac.cn>
2021-12-31 19:32:24 +0800

    Fit VS-C-R57.A1.

--- make_portable.ps1
+++ make_portable.ps1
@@ -83,6 +83,7 @@ Pop-Location
 $Requirements = Get-Item .\downloads\vspreview\vapoursynth-preview-$($Packages.vspreview.branch)\requirements.txt
 Set-Content -Path $Requirements (Get-Content -Path $Requirements | Select-String -Pattern 'vapoursynth' -NotMatch )
 .\VapourSynth\python.exe -m pip install -r $Requirements --no-warn-script-location
+.\VapourSynth\python.exe -m pip install flake8 black --no-warn-script-location
 .\VapourSynth\python.exe -m pip install .\downloads\vsstubs\VapourSynth-Plugins-Stub-Generator-$($Packages.vsstubs.branch)\vsstubs\ --no-warn-script-location
 .\VapourSynth\python.exe -m vsstubs install
 
@@ -91,7 +92,7 @@ if ( Test-Path -Path .\VapourSynth\__pycache__ ) {
     Remove-Item -Path .\VapourSynth\__pycache__ -Recurse -Force
 }
 Move-Item -Path .\VapourSynth\sitecustomize.py -Destination .\VapourSynth\Lib\ -Force
-Move-Item -Path .\VapourSynth\vapoursynth.cp*.pyd -Destination .\VapourSynth\Lib\site-packages\ -Force
+Move-Item -Path .\VapourSynth\vapoursynth.cp39-win_amd64.pyd -Destination .\VapourSynth\Lib\site-packages\ -Force
 Copy-Item -Path .\downloads\vspreview\vapoursynth-preview-$($Packages.vspreview.branch)\vspreview -Destination .\VapourSynth\Lib\site-packages\ -Recurse -Force
 # Copy-Item -Path .\downloads\VSRepoGUI\VSRepoGUI.exe -Destination .\VapourSynth\ -Force
 Copy-Item -Path .\vsrepogui.json -Destination .\VapourSynth\ -Force
@@ -111,7 +112,7 @@ Pop-Location
 Remove-Item -Path .\VapourSynth\LICENSE.txt
 Remove-Item -Path .\VapourSynth\setup.py, .\VapourSynth\MANIFEST.in
 Remove-Item -Path .\VapourSynth\VapourSynth_portable.egg-info -Recurse
-Remove-Item -Path .\VapourSynth\vs-detect-python.bat, .\VapourSynth\VSScriptPython38.dll
+Remove-Item -Path .\VapourSynth\vs-detect-python.bat, .\VapourSynth\vapoursynth.cp38-win_amd64.pyd
 Remove-Item -Path .\VapourSynth\README, .\VapourSynth\CHANGELOG, .\VapourSynth\LICENSE
 Remove-Item -Path .\VapourSynth\vsedit.ico, .\VapourSynth\vsedit.svg
 

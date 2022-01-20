eaa88054887d558f6a4e8a19b49fb56f10ef7d77
SaltyChiang <jiangxiangyu@ihep.ac.cn>
2022-01-20 14:52:16 +0800

    Move VapourSynthScripts folder to vapoursynth64\scripts.

--- make_portable.ps1
+++ make_portable.ps1
@@ -86,7 +86,7 @@ Copy-Item -Path .\downloads\vspreview\vapoursynth-preview-$($Packages.vspreview.
 Copy-Item -Path .\vsrepogui.json -Destination .\VapourSynth\ -Force
 Copy-Item -Path .\vsedit.config -Destination .\VapourSynth\VapourSynthEditor\ -Force
 New-Item -Path .\VapourSynth\vsedit.config -Force | Out-Null
-New-Item -Path .\VapourSynth\VapourSynthScripts -ItemType Directory -Force | Out-Null
+New-Item -Path .\VapourSynth\vapoursynth64\scripts -ItemType Directory -Force | Out-Null
 
 
 Push-Location -Path downloads
